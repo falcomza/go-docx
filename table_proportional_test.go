7a1ffaf60dfb1e8a265d4b265ecd536065bb709e
falcomza <falcomza@gmail.com>
2026-04-01 21:34:30 +0200

    feat: add support for embedded Excel files and improve content type handling

--- table_proportional_test.go
+++ table_proportional_test.go
@@ -11,24 +11,20 @@ import (
 
 // extractGridColumnWidths extracts grid column widths from document XML
 func extractGridColumnWidths(docXML string) []int {
-	tblStart := strings.Index(docXML, "<w:tbl>")
-	if tblStart == -1 {
+	// Find the LAST table's grid, not the first
+	// This is important for tests that insert tables into templates with existing tables
+	lastTblGridStart := strings.LastIndex(docXML, "<w:tblGrid>")
+	if lastTblGridStart == -1 {
 		return nil
 	}
 
-	tblGridStart := strings.Index(docXML[tblStart:], "<w:tblGrid>")
-	if tblGridStart == -1 {
-		return nil
-	}
-	tblGridStart += tblStart
-
-	tblGridEnd := strings.Index(docXML[tblGridStart:], "</w:tblGrid>")
+	tblGridEnd := strings.Index(docXML[lastTblGridStart:], "</w:tblGrid>")
 	if tblGridEnd == -1 {
 		return nil
 	}
-	tblGridEnd += tblGridStart
+	tblGridEnd += lastTblGridStart
 
-	gridXML := docXML[tblGridStart : tblGridEnd+len("</w:tblGrid>")]
+	gridXML := docXML[lastTblGridStart : tblGridEnd+len("</w:tblGrid>")]
 	return extractGridColumns(gridXML)
 }
 
